bcb476b08424eb06ad31310be14aa75286390f9c
Conrelia Harder <cornelialisaharder@gmail.com>
2022-09-01 09:08:01 +0200

    Ökat till fem rum

--- public/modules/user.js
+++ public/modules/user.js
@@ -4,20 +4,54 @@ function userJoin(users, id, username, room) {
     playRoom = room;
   } else if (users.length > 3 && users.length <= 7) {
     playRoom = room + "1";
-  } else if (users.length >= 7) {
+  } else if (users.length > 7 && users.length <= 11) {
     playRoom = room + "2";
+  } else if (users.length > 11 && users.length <= 15) {
+    playRoom = room + "3";
+  } else if (users.length > 15 && users.length <= 19) {
+    playRoom = room + "4";
+  } else if (users.length > 19) {
+    playRoom = room + "5";
   }
 
-  if (users.length === 0 || users.length === 4 || users.length === 8) {
+  if (
+    users.length === 0 ||
+    users.length === 4 ||
+    users.length === 8 ||
+    users.length === 12 ||
+    users.length === 16 ||
+    users.length === 20
+  ) {
     //blue
     userColor = "#0000FF";
-  } else if (users.length === 1 || users.length === 5 || users.length === 9) {
+  } else if (
+    users.length === 1 ||
+    users.length === 5 ||
+    users.length === 9 ||
+    users.length === 13 ||
+    users.length === 17 ||
+    users.length === 21
+  ) {
     //green
     userColor = "#008000";
-  } else if (users.length === 2 || users.length === 6 || users.length === 10) {
+  } else if (
+    users.length === 2 ||
+    users.length === 6 ||
+    users.length === 10 ||
+    users.length === 14 ||
+    users.length === 18 ||
+    users.length === 22
+  ) {
     //yellow
     userColor = "#FFFF00";
-  } else if (users.length === 3 || users.length === 7 || users.length === 11) {
+  } else if (
+    users.length === 3 ||
+    users.length === 7 ||
+    users.length === 11 ||
+    users.length === 15 ||
+    users.length === 19 ||
+    users.length === 23
+  ) {
     //red
     userColor = "#FF0000";
   }
